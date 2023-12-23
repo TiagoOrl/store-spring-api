abb5a0c2ac3a2ee8600250c3d230d5c7cce32613
TiagoOrl <tiago_orlando@yahoo.com>
2023-12-22 21:28:34 -0300

    refactor: exception handler middleware

--- src/main/java/com/asm/estore/middleware/ExceptionMainHandler.java
+++ src/main/java/com/asm/estore/middleware/ExceptionMainHandler.java
@@ -3,16 +3,19 @@ package com.asm.estore.middleware;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice
-public class ExceptionHandler {
+@ControllerAdvice
+public class ExceptionMainHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     protected Map<String, String> handleValidationExceptions(
             MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
