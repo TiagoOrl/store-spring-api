75a45a1a4ffe1527edb6b948d4118de2e5cdb70c
TiagoOrl <tiago_orlando@yahoo.com>
2023-12-28 20:23:34 -0300

    perf: imeplemented caching with Redis

--- src/main/java/com/asm/estore/EstoreApplication.java
+++ src/main/java/com/asm/estore/EstoreApplication.java
@@ -2,8 +2,10 @@ package com.asm.estore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class EstoreApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/asm/estore/dto/order/OrderDTO.java
+++ src/main/java/com/asm/estore/dto/order/OrderDTO.java
@@ -1,11 +1,12 @@
 package com.asm.estore.dto.order;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
-public class OrderDTO {
+public class OrderDTO implements Serializable {
     private Integer id;
     private BigDecimal totalSum;
     private Date createdAt;
