213698163ae58ef1345e0abb200ad0c63ebef766
Tiago Orlando <tiago_orlando@yahoo.com>
2024-02-12 13:49:14 -0300

    docs: added schemas

---
+++ schemas/insert_orders.sql
@@ -0,0 +1,23 @@
+-- Orders
+
+
+INSERT INTO order_product (
+	fk_order_id, fk_product_id, amount
+)
+VALUES (
+	1, 1, 2
+);
+
+INSERT INTO order_product (
+	fk_order_id, fk_product_id, amount
+)
+VALUES (
+	1, 7, 3
+);
+
+INSERT INTO order_product (
+	fk_order_id, fk_product_id, amount
+)
+VALUES (
+	1, 4, 1
+);
---
+++ src/test/java/com/asm/estore/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.asm.estore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ExampleTest {
+    @Test
+    public void testing(){
+
+    }
+}
