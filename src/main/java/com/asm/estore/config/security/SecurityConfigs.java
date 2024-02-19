2edb6dd8350ef76cec7593de1bef46648c9649ff
Tiago Orlando <tiago_orlando@yahoo.com>
2024-02-19 19:42:21 -0300

    fix: adjusting role filters for controllers

--- src/main/java/com/asm/estore/config/security/SecurityConfigs.java
+++ src/main/java/com/asm/estore/config/security/SecurityConfigs.java
@@ -3,6 +3,7 @@ package com.asm.estore.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +18,12 @@ public class SecurityConfigs {
                  .csrf(csrf -> csrf.disable())
                  .sessionManagement(session ->
                          session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                 )
+                 .authorizeHttpRequests(auth -> auth
+                         .requestMatchers("/api/admin/**").hasRole("admin")
+                         .requestMatchers("/api/user/**").hasRole("client")
+                         .requestMatchers("/api/client/create").permitAll()
+                         .anyRequest().permitAll()
                  )
                  .build();
      }
