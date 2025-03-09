c785d8dab3a56dd7b943cba73f955aa6ff634841
TiagoOrl <tiago_orlando@yahoo.com>
2025-03-09 13:29:51 -0300

    updated insert products script

--- src/main/java/com/asm/estore/controller/ProductController.java
+++ src/main/java/com/asm/estore/controller/ProductController.java
@@ -14,10 +14,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/public/product")
-@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
     private final ProductService productService;
 
